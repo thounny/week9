9829bf11ab7b94e432732e63e7de1615584a8d20
thounny <thounnykeo@gmail.com>
2024-11-13 13:25:30 -0500

    testMe function

--- testing.js
+++ testing.js
@@ -4,6 +4,21 @@ function testMe(a, b) {
 
 // console.log(testMe(1, 2)); // expected output: 3
 testData = [
-  { inputs: [6, 7], output: [13] },
-  { inputs: [12, 14], output: [25] },
+  { inputs: [6, 7], output: 13 },
+  { inputs: [12, 14], output: 25 },
+  { inputs: [12, 13], output: 24 },
 ];
+
+function runner(fn, ioList) {
+  ioList.forEach((test, i) => {
+    result = fn(...test.inputs); //... spread ,
+    success = result === test.output; // match expected output
+    if (success) {
+      console.log("PASS", i);
+    } else {
+      console.log("FAIL", i);
+    }
+  });
+}
+
+runner(testMe, testData);
