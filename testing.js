0a15d6329751d454471992de9e5f47c00c0aa221
thounny <thounnykeo@gmail.com>
2024-11-13 13:14:52 -0500

    Week 9

---
+++ testing.js
@@ -0,0 +1,9 @@
+function testMe(a, b) {
+  return a + b;
+}
+
+// console.log(testMe(1, 2)); // expected output: 3
+testData = [
+  { inputs: [6, 7], output: [13] },
+  { inputs: [12, 14], output: [25] },
+];
